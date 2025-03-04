062ec44eef847bc580a2734c2bcb2b9319850145
Faizan3294 <faziraja3294@gmail.com>
2025-03-04 23:57:37 +0500

    File name changed

---
+++ Messaging App/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Messaging App
+//
+//  Created by Faizan on 04/03/2025.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
