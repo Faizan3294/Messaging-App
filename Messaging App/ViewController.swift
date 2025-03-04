4080be1428cf4dfc222e035ab7e9c2cc1d5b51ad
Faizan3294 <faziraja3294@gmail.com>
2025-03-04 23:40:36 +0500

    Initial Commit

---
+++ Messaging App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messaging App
+//
+//  Created by Faizan on 04/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
